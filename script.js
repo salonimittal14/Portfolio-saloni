a658c48ae6becb2236057fc5e6b92a9d42bf3424
salonimittal14 <93769831+salonimittal14@users.noreply.github.com>
2024-08-26 14:18:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// TYPING ANIMATION
+var typed = new Typed(".typing", {
+    strings: ["", "Web Designer", "Python Learner", "Artificial Intelligence Beginner"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+// aside//
+const nav = document.querySelector(".nav"),
+navList = nav.querySelectorAll("li"),
+totalNavList = navList.length;
+for(let i=0; i<totalNavList; i++)
+{
+const a = navList[i].querySelector("a");
+a.addEventListener("click", function()
+{
+    for(let j=0;j<totalNavList;j++)
+    {
+        navList[j].querySelector("a").classList.remove("active");
+    }
+this.classList.add("active")
+showSection(this);
+})
+
+}
